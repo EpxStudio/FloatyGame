77d155c24a740946c32d794dc8d3f269f2260f85
Ian Wold <ilwold@hotmail.com>
2016-10-14 22:18:42 -0500

    Background and Time
    Added TimeScript, time moves!
    And the background moves with it! WOOOO

---
+++ TreeConcept/Assets/BackgroundScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class BackgroundScript : MonoBehaviour
+{
+	// Use this for initialization
+	void Start() { }
+
+	float lastTick = 0f;
+
+	// Update is called once per frame
+	void Update()
+	{
+		float currentTick = TimeScript.GetTick();
+		var tickDifference = currentTick - lastTick;
+		tickDifference = tickDifference < 0 ? currentTick : tickDifference;
+		lastTick = currentTick;
+
+		transform.Rotate(0f, 0f, tickDifference / 1000f);
+	}
+}
---
+++ TreeConcept/Assets/Scripts/TimeScript.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+
+public class TimeScript : MonoBehaviour
+{
+	static DateTime StartTime;
+	public static int MinutesPerDay = 3;
+	
+	// Use this for initialization
+	void Start()
+	{
+		StartTime = DateTime.Now;
+		Debug.Log("Started TimeScript");
+	}
+
+	// Update is called once per frame
+	void Update() { }
+
+	public static int GetDay()
+	{
+		return Convert.ToInt32(
+			Math.Floor(
+				Convert.ToDouble(
+					DateTime.Now.Subtract(StartTime).Minutes / MinutesPerDay
+				)
+			)
+		);
+	}
+
+	//There are 18000 ticks in a day
+	//9000 ticks for day and 9000 ticks for night
+	//150 ticks per real-world second
+	public static int GetTick()
+	{
+		var sub = DateTime.Now.Subtract(StartTime);
+		return Convert.ToInt32(
+			Math.Floor(
+				Convert.ToDouble(
+					(sub.Milliseconds + (1000 * sub.Seconds) +
+						((1000 * MinutesPerDay) *
+							(sub.Minutes - (MinutesPerDay *
+								Math.Floor(Convert.ToDouble(sub.Minutes / MinutesPerDay))
+							))
+						)
+					) / 10
+				)
+			)
+		);
+	}
+
+	public static int GetTotalTick()
+	{
+		return (18000 * GetDay()) + GetTick();
+	}
+}
